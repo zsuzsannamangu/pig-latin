7f77d4b99fba8378c231350bf8da3d03b94829cc
Reese Lee and Zsuzsanna Mangu <zsuzsanna@>
2019-04-03 16:16:03 -0700

    add javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 // business logic
 // var english = function(engInput) {
-//   if((engInput = vowels) || (engInput = consonants)) {
-//     return true;
-//   } else
-//     return false;
-//   };
+  // if((engInput === vowels) || (engInput === consonants)) {
+  //   return true;
+  // } else
+  //   return false;
+  // };
 
 var vowels = ["a", "e", "i", "o", "u"];
-// var consonants != vowels;
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var alphabet = ["a", "e", "i", "o", "u", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
-var index = 0;
+function isAVowel(letter) {
+  for (var i = 0; i  < english.length; i++) {
+    if (letter === vowels[i]) return true;
+  }
+  return false;
+}
+var i = 0;
+
+function isANumber(number) {
+  for (var i = 0; i < english.length; i++) {
+    if (number!==alphabet[i]) return true;
+  } return true;
+}
 
 // if(engInput[index] === vowels) {
 //   //add "way" to the end
@@ -26,27 +39,48 @@ $(function() {
   $("form#enterEnglish").submit(function(event) {
     event.preventDefault();
     english = $("#english").val();
-    index = 0;
-    if (vowels.includes(english[index])) {
-      //add "way" to the end
+    i = 0;
+    if (isAVowel(english[i])) {
+      //add "way" to the end for words beginning with vowels
       $(".translation").text(english.concat("way"));
-    } else if (english[index] != vowels) {
-      //remove first letter to the end
-      $(".translation").text(english.substr(1)+english.substr(0,1)+"ay");
+    } else if (consonants.includes(english[i])) {
+      $(".translation").text(english.slice(1)+english.slice(0,1)+"ay");
     } else {
-      return false;
-    }
+     return english;
+     }
+
+
+
+  
 
     $("#result").show();
-
-
-    if (vowels.includes(english[index])){
-      english.concat("way");
-    }
-
-
-
   });
 
-
 });
+
+
+    //
+    // if (consonants.includes(english[index])) {
+    //   $(".translation").text(english.substr(1)+english.substr(0,1)+"ay");
+    // } else if (english.includes(vowels) || english.includes(consonants)){
+    //   return false;
+    //   alert("enter only words");
+    // } else {
+    //   alert("error");
+
+
+
+// if (english.includes(vowels) || english.includes(consonants))
+
+
+//
+// (vowels.!includes(english))
+// (consonants.!includes(english))
+
+
+    // if (vowels.includes(english[index])){
+    //   english.concat("way");
+    // }
+
+
+// if (english != vowels || english != consonants){
