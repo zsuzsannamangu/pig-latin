8053c746a8fe378598a5846965e5408496813387
Reese Lee and Zsuzsanna Mangu <zsuzsanna@>
2019-04-03 11:45:17 -0700

    add initial variables

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 var english = function(engInput) {
   if((engInput = vowels) || (engInput = consonants)) {
     return true;
-  } else {
+  } else
     return false;
   }
 };
@@ -10,6 +10,17 @@ var english = function(engInput) {
 var vowels = ["a", "e", "i", "o", "u"];
 var consonants != vowels;
 
+var index = 0;
+
+if(english[index] === vowels) {
+  //add "way" to the end
+  english.concat("way");
+} else
+
+if(english[index] != vowels) {
+  //remove first letter to the end
+  english.slice(0).
+}
 
 
 
