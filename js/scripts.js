7a835b6c1761b354998dc4e6917f0d202c8ff2d4
Reese Lee and Zsuzsanna Mangu <zsuzsanna@>
2019-04-03 10:50:41 -0700

    add javascript business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 // business logic
+var english = function(engInput) {
+  if((engInput = vowels) || (engInput = consonants)) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
+var vowels = ["a", "e", "i", "o", "u"];
+var consonants != vowels;
 
 
 
 
 
 // user interface logic
+$(function() {
+  $("form#enterEnglish").submit(function(event) {
+    event.preventDefault();
+
+
+
+
+  }
+
+
+})
