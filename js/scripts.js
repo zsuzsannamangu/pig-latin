712515d3f83a5ee22fc7b5fb94061e8a6d1970c3
Reese Lee and Zsuzsanna Mangu <reese@>
2019-04-03 13:58:40 -0700

    fix javascript bug for if statements, have first two conditions in place.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,52 @@
 // business logic
-var english = function(engInput) {
-  if((engInput = vowels) || (engInput = consonants)) {
-    return true;
-  } else
-    return false;
-  }
-};
+// var english = function(engInput) {
+//   if((engInput = vowels) || (engInput = consonants)) {
+//     return true;
+//   } else
+//     return false;
+//   };
 
 var vowels = ["a", "e", "i", "o", "u"];
-var consonants != vowels;
+// var consonants != vowels;
 
 var index = 0;
 
-if(english[index] === vowels) {
-  //add "way" to the end
-  english.concat("way");
-} else
-
-if(english[index] != vowels) {
-  //remove first letter to the end
-  english.slice(0).
-}
-
-
-
+// if(engInput[index] === vowels) {
+//   //add "way" to the end
+//   english.concat("way");
+// } else
+//
+// if(engInput[index] != vowels) {
+//   //remove first letter to the end
+//   english.substr(1)+english.substr(0,1)+"ay";
+// }
 
 // user interface logic
 $(function() {
   $("form#enterEnglish").submit(function(event) {
     event.preventDefault();
+    english = $("#english").val();
+    index = 0;
+    if (vowels.includes(english[index])) {
+      //add "way" to the end
+      $(".translation").text(english.concat("way"));
+    } else if (english[index] != vowels) {
+      //remove first letter to the end
+      $(".translation").text(english.substr(1)+english.substr(0,1)+"ay");
+    } else {
+      return false;
+    }
+
+    $("#result").show();
+
+
+    if (vowels.includes(english[index])){
+      english.concat("way");
+    }
 
 
 
+  });
 
-  }
 
-
-})
+});
